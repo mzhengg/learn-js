48ccf7deddbbe8f8aefecd726d2b8fc99c260f61
Michael Zheng <mzheng@Michaels-MacBook-Pro.local>
2024-02-01 13:33:52 -0500

    array and hideemail quizzes done

---
+++ quiz/scripts/hideEmail-q.js
@@ -0,0 +1,16 @@
+/* anonymizes the username(local part) of an email ID, that is, 
+replace the username part with * characters.
+*/
+const emails = ['mary@northeastern.edu', 'ari@khoury.northeastern.edu', 'jk_neu.edu', 'jk@neu.edu', 'jsned@', 'ai_me@mugar.northeastern.edu'];
+window.onload = function hideEmail() {
+    const list = document.getElementById('emails')
+    list.innerHTML = '';
+    for (const email of emails) {
+        if (email.match(/^[a-z0-9_]+@(.+\.)*northeastern.edu$/)) {
+            console.log(email);
+            const listItem = document.createElement('li');
+            listItem.textContent = email.slice(0, email.indexOf('@')).replace(/./g, '*') + email.slice(email.indexOf('@'));
+            list.appendChild(listItem);
+        }
+    }
+}
