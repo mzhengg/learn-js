3cd2a372b46ce8751fba4f63de97b3f5dde96cc7
Michael Zheng <mzheng@Michaels-MacBook-Pro.local>
2024-02-05 16:21:30 -0500

    finished exception, map-deep, and stack quizzes

---
+++ quiz/scripts/stack-q1.js
@@ -0,0 +1,41 @@
+class PStack {
+  #id;
+  constructor() {
+    this.#id = 1;
+    this._persons = [];
+  }
+
+  showId() {
+    return this.#id;
+  }
+
+}
+
+class PStackImpl extends PStack {
+  constructor() {
+    super();
+  }
+
+  get persons() {
+    return this._persons
+  }
+
+  set persons(persons) {
+    this._persons = persons
+  }
+
+  push(p) {
+    return this._persons.push(p)
+  }
+
+  pop() {
+    return this._persons.pop().age
+  }
+}
+
+let pstack = new PStackImpl();
+pstack.persons = [{name: 'Jojo', age: 21}, {name: 'Gabi', age: 29}]
+pstack.push({name: 'Dein', age: 19});
+console.log(pstack.pop());
+console.log(pstack.pop());
+console.log(pstack.persons);
